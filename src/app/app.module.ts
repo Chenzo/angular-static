b055a4df64f3eb16053624073de995fa5a3145f2
Vince Suriani <chenzorama@hotmail.com>
2020-12-28 15:11:41 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+
+import { RouterModule, Routes } from '@angular/router';
+import { PageoneComponent } from './pageone/pageone.component';
+import { PagetwoComponent } from './pagetwo/pagetwo.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'pageone', component: PageoneComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PageoneComponent,
+    PagetwoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
